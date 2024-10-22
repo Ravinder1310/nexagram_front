386e349b9ecaff8ff2957e1fac90f251f58a5d70
sverma507 <sverma70568@gmail.com>
2024-10-22 20:26:12 +0530

    new

--- src/components/CommentDialog.jsx
+++ src/components/CommentDialog.jsx
@@ -34,7 +34,7 @@ const CommentDialog = ({ open, setOpen }) => {
   const sendMessageHandler = async () => {
     try {
       const res = await axios.post(
-        `http://localhost:3000/api/v1/post/${selectedPost?._id}/comment`,
+        `${import.meta.env.VITE_API_URL}/api/v1/post/${selectedPost?._id}/comment`,
         { text },
         {
           headers: {
