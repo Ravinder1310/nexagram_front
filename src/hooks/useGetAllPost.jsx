ba435e7561ef8294292709b82cc4c42232c69e8e
sverma507 <sverma70568@gmail.com>
2024-10-22 11:34:27 +0530

    new

--- src/hooks/useGetAllPost.jsx
+++ src/hooks/useGetAllPost.jsx
@@ -9,11 +9,7 @@ const useGetAllPost = () => {
     useEffect(() => {
         const fetchAllPost = async () => {
             try {
-                const res = await axios.get(`${import.meta.env.VITE_API_URL}/api/v1/post/all`, {headers: {
-                    'Content-Type': 'application/json'
-                },
-                withCredentials: true
-            });
+                const res = await axios.get(`${import.meta.env.VITE_API_URL}/api/v1/post/all`, { withCredentials: true });
                 if (res.data.success) { 
                     console.log(res.data.posts);
                     dispatch(setPosts(res.data.posts));
