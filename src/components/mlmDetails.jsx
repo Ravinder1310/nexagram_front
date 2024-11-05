205b14b94b45d65e156f39e355ca53081f426de2
Ravinder1310 <105969256+Ravinder1310@users.noreply.github.com>
2024-11-05 12:41:47 +0530

    added

--- src/components/mlmDetails.jsx
+++ src/components/mlmDetails.jsx
@@ -2,12 +2,24 @@ import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { toast, Toaster } from "react-hot-toast";
 import { Facebook, Twitter, Instagram, Linkedin } from "lucide-react";
-import { FaWhatsapp } from "react-icons/fa";
+import { FaWhatsapp, FaTrophy, FaAward } from "react-icons/fa";
 import investLogo from "./images/invest.png";
 import { motion } from "framer-motion";
+import sapphire from "./images/ranks/1.png";
+import ruby from "./images/ranks/2.png";
+import topaz from "./images/ranks/3.png";
+import emerald from "./images/ranks/4.png";
+import platinum from "./images/ranks/5.png";
+import diamond from "./images/ranks/6.png";
+import greenDiamond from "./images/ranks/7.png";
+import blueDiamond from "./images/ranks/8.png";
+import blackDiamond from "./images/ranks/9.png";
+import goldDiamond from "./images/ranks/10.png";
+
+// import { BadgeCheckIcon } from '@heroicons/react/solid';
 
 const AnimatedBorderBox = ({ children }) => (
-  <div className="relative p-[3px] rounded-lg overflow-hidden mt-10">
+  <div className="relative p-[2px] rounded-lg overflow-hidden mt-10 mb-10">
     {/* Rotating Background */}
     <motion.div
       className="absolute inset-0 -z-10"
@@ -37,7 +49,120 @@ const AnimatedBorderBox = ({ children }) => (
 const MlmDetails = () => {
   const [isLinkCopied, setIsLinkCopied] = useState(false);
   const [invitationLink, setInvitationLink] = useState("");
+  const [selectedRank, setSelectedRank] = useState(0);
   const { user } = useSelector((store) => store.auth);
+  const [isOpen, setIsOpen] = useState(false);
+  const openModal = () => setIsOpen(true);
+  const closeModal = () => setIsOpen(false);
+
+  const rankRequirements = [
+    {
+      rank: "saphire",
+      logo: sapphire,
+      self: 250,
+      directTeam: 5,
+      directBusiness: 1000,
+      totalTeamSize: 20,
+      teamBusiness: 10000,
+      reward: 250,
+    },
+    {
+      rank: "rubi",
+      logo: ruby,
+      self: 500,
+      directTeam: 7,
+      directBusiness: 1500,
+      totalTeamSize: 50,
+      teamBusiness: 30000,
+      reward: 750,
+    },
+    {
+      rank: "topaz",
+      logo: topaz,
+      self: 1000,
+      directTeam: 9,
+      directBusiness: 2000,
+      totalTeamSize: "2 Ruby",
+      teamBusiness: "Any",
+      reward: 3000,
+    },
+    {
+      rank: "emerald",
+      logo: emerald,
+      self: 1500,
+      directTeam: 11,
+      directBusiness: 3000,
+      totalTeamSize: "2 Topaz",
+      teamBusiness: "Any",
+      reward: 10000,
+    },
+    {
+      rank: "platinum",
+      logo: platinum,
+      self: 2000,
+      directTeam: 15,
+      directBusiness: 3500,
+      totalTeamSize: "2 Emerald",
+      teamBusiness: "Any",
+      reward: 25000,
+    },
+    {
+      rank: "diamond",
+      logo: diamond,
+      self: 2500,
+      directTeam: 17,
+      directBusiness: 4000,
+      totalTeamSize: "2 Platinum",
+      teamBusiness: "Any",
+      reward: 7500,
+    },
+    {
+      rank: "green diamond",
+      logo: greenDiamond,
+      self: 3000,
+      directTeam: 15,
+      directBusiness: 4500,
+      totalTeamSize: "2 Diamonds",
+      teamBusiness: "Any",
+      reward: 225000,
+    },
+    {
+      rank: "blue diamond",
+      logo: blueDiamond,
+      self: 3500,
+      directTeam: 21,
+      directBusiness: 5000,
+      totalTeamSize: "2 Green Diamond",
+      teamBusiness: "Any",
+      reward: 700000,
+    },
+    {
+      rank: "black diamond",
+      logo: blackDiamond,
+      self: 4000,
+      directTeam: 23,
+      directBusiness: 5500,
+      totalTeamSize: "3 Blue Diamond",
+      teamBusiness: "Any",
+      reward: 900000,
+    },
+    {
+      rank: "crown",
+      logo: goldDiamond,
+      self: 5000,
+      directTeam: 25,
+      directBusiness: 10000,
+      totalTeamSize: "3 Black Diamond",
+      teamBusiness: "Any",
+      reward: 1100000,
+    },
+  ];
+
+  const handleRank = (rank) => {
+    console.log(rank);
+    setSelectedRank(rank);
+    setIsOpen(true);
+  };
 
   const generateInvitationLink = () => {
     const link = `${window.location.origin}/invester-registration?referral=${user.referralCode}`;
@@ -230,6 +355,205 @@ const MlmDetails = () => {
             </div>
           </div>
         </div>
+      </AnimatedBorderBox>
+      <AnimatedBorderBox>
+        <h1 className="font-bold text-md text-gray-500 mt-3">
+          Rank Details (Team Business Rewards)
+        </h1>
+        <div
+          className="flex p-2 gap-2 overflow-x-auto mt-4"
+          style={{ scrollbarWidth: "none", msOverflowStyle: "none" }} // For Firefox and IE/Edge
+        >
+          <style>
+            {`
+      /* Hide scrollbar for Chrome, Safari, and Opera */
+      .hide-scrollbar::-webkit-scrollbar {
+        display: none;
+      }
+    `}
+          </style>
+
+          <div className="text-center w-[120px] h-[auto] flex-shrink-0 relative">
+            <img
+              src={sapphire}
+              className="h-[120px] grayscale"
+              onClick={() => {
+                handleRank(0);
+              }}
+              alt="error"
+            />
+            <h1 className="font-mono text-lg mt-3 text-gray-500">Sapphire</h1>
+          </div>
+          <div className="text-center w-[120px] h-[auto] flex-shrink-0 relative">
+            <img
+              src={ruby}
+              className="h-[120px] grayscale"
+              alt="error"
+              onClick={() => {
+                handleRank(1);
+              }}
+            />
+            <h1 className="font-mono text-lg mt-3 text-gray-500">Ruby</h1>
+          </div>
+          <div className="text-center w-[120px] h-[auto] flex-shrink-0 relative">
+            <img
+              src={topaz}
+              className="h-[120px] grayscale"
+              alt="error"
+              onClick={() => {
+                handleRank(2);
+              }}
+            />
+            <h1 className="font-mono text-lg mt-3 text-gray-500">Topaz</h1>
+          </div>
+          <div className="text-center w-[120px] h-[auto] flex-shrink-0 relative">
+            <img
+              src={emerald}
+              className="h-[120px] grayscale"
+              alt="error"
+              onClick={() => {
+                handleRank(3);
+              }}
+            />
+            <h1 className="font-mono text-lg mt-3 text-gray-500">Emerald</h1>
+          </div>
+          <div className="text-center w-[120px] h-[auto] flex-shrink-0 relative">
+            <img
+              src={platinum}
+              className="h-[120px] grayscale"
+              alt="error"
+              onClick={() => {
+                handleRank(4);
+              }}
+            />
+            <h1 className="font-mono text-lg mt-3 text-gray-500">Platinum</h1>
+          </div>
+          <div className="text-center w-[120px] h-[auto] flex-shrink-0 relative">
+            <img
+              src={diamond}
+              className="h-[120px] grayscale"
+              alt="error"
+              onClick={() => {
+                handleRank(5);
+              }}
+            />
+            <h1 className="font-mono text-lg mt-3 text-gray-500">Diamond</h1>
+          </div>
+          <div className="text-center w-[120px] h-[auto] flex-shrink-0 relative">
+            <img
+              src={greenDiamond}
+              className="h-[120px] grayscale"
+              alt="error"
+              onClick={() => {
+                handleRank(6);
+              }}
+            />
+            <h1 className="font-mono text-lg mt-3 text-gray-500">
+              Green Diamond
+            </h1>
+          </div>
+          <div className="text-center w-[120px] h-[auto] flex-shrink-0 relative">
+            <img
+              src={blueDiamond}
+              className="h-[120px] grayscale"
+              alt="error"
+              onClick={() => {
+                handleRank(7);
+              }}
+            />
+            <h1 className="font-mono text-lg mt-3 text-gray-500">
+              Blue Diamond
+            </h1>
+          </div>
+          <div className="text-center w-[120px] h-[auto] flex-shrink-0 relative">
+            <img
+              src={blackDiamond}
+              className="h-[120px] grayscale"
+              alt="error"
+              onClick={() => {
+                handleRank(8);
+              }}
+            />
+            <h1 className="font-mono text-lg mt-3 text-gray-500">
+              Black Diamond
+            </h1>
+          </div>
+          <div className="text-center w-[120px] h-[auto] flex-shrink-0 relative">
+            <img
+              src={goldDiamond}
+              className="h-[120px] grayscale"
+              alt="error"
+              onClick={() => {
+                handleRank(9);
+              }}
+            />
+            <h1 className="font-mono text-lg mt-3 text-gray-500">
+              Crown Diamond
+            </h1>
+          </div>
+        </div>
+        {isOpen && (
+          <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+            <div className="bg-white p-6 rounded-lg w-[300px] text-center">
+              <h2 className="text-xl font-bold mb-4">Rank Requirements</h2>
+              {/* <p className="text-gray-700 mb-4 text-lg font-serif">
+              Here are the requirements or conditions to achieve the Sapphire rank:
+            </p> */}
+              <img
+                src={rankRequirements[selectedRank]?.logo}
+                className="w-[100px] m-auto mb-4"
+                alt="error"
+              />
+              {/* Add your requirements or conditions here */}
+              <ul className="list-disc list-inside text-gray-600 text-left">
+                <li className="font-bold">
+                  Self Activation :{" "}
+                  <span className="text-blue-500">
+                    $ {rankRequirements[selectedRank]?.self}
+                  </span>
+                </li>
+                <li className="font-bold">
+                  Direct Team :{" "}
+                  <span className="text-blue-500">
+                    {rankRequirements[selectedRank]?.directTeam}
+                  </span>
+                </li>
+                <li className="font-bold">
+                  Direct Business :{" "}
+                  <span className="text-blue-500">
+                    $ {rankRequirements[selectedRank]?.directBusiness}{" "}
+                  </span>
+                </li>
+                <li className="font-bold">
+                  Team Size :{" "}
+                  <span className="text-blue-500">
+                    {rankRequirements[selectedRank]?.totalTeamSize}
+                  </span>
+                </li>
+                <li className="font-bold">
+                  Team Business :{" "}
+                  <span className="text-blue-500">
+                    $ {rankRequirements[selectedRank]?.teamBusiness}
+                  </span>
+                </li>
+              </ul>
+
+              <h1 className="text-center mt-4 font-bold">
+                Reward you get from this:{" "}
+                <span className="text-blue-500">
+                  $ {rankRequirements[selectedRank]?.reward}
+                </span>
+              </h1>
+
+              <button
+                className=" mt-4 bg-blue-500 text-white px-4 py-2 rounded"
+                onClick={closeModal}
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        )}
       </AnimatedBorderBox>
     </div>
   );
